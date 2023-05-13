1beccf4c0aa8cf3720f8b6a47142d49f5c421876
Julia Simon <juli.simonova@gmail.com>
2023-05-13 18:26:50 +0300

    added test

--- src/test/java/quru/qu/FileParsingTest.java
+++ src/test/java/quru/qu/FileParsingTest.java
@@ -1,2 +1,32 @@
-package quru.qu;public class FileParsingTest {
+package quru.qu;
+
+import com.codeborne.xlstest.XLS;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class FileParsingTest {
+
+    private ClassLoader cl = FileParsingTest.class.getClassLoader();
+
+
+    @Test
+    void xlsxZipTest() throws Exception {
+        try (InputStream is = cl.getResourceAsStream("forTest.zip")) {
+            try (ZipInputStream zs = new ZipInputStream(is)) {
+                ZipEntry entry;
+                while ((entry = zs.getNextEntry()) != null) {
+                    if (entry.getName().equals("xlsxTest.xlsx")) {
+                        XLS xls = new XLS(zs);
+                        assertTrue(xls.excel.getSheetAt(0).getRow(1).getCell(3).getStringCellValue().startsWith("123vCWvCaNKB2Gx9NyBfPpieb9Dl7i@6pa3JWpzfs.com"));
+                    }
+                }
+
+            }
+        }
+
+    }
 }
