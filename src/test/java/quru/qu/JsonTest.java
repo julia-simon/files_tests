7c8e58f3686d61fc1d28a8bc6051825dd7477e34
Julia Simon <juli.simonova@gmail.com>
2023-05-14 17:27:25 +0300

    added files

--- src/test/java/quru/qu/JsonTest.java
+++ src/test/java/quru/qu/JsonTest.java
@@ -1,2 +1,10 @@
-package quru.qu;public class jsonTest {
+package quru.qu;
+
+import java.util.List;
+
+public class JsonTest {
+    public String action;
+    public String person_id;
+    public Integer id;
+    public List<String> entity_name;
 }
